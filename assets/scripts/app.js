98fe9fa2d5d12ec4d3c300713ae7225b6a4ae5fd
Christine <huangc909@gmail.com>
2020-07-05 14:52:15 -0400

    Changed go-back-to-pc from id to class Added in the go-back-to-pc in add plant page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -25,7 +25,7 @@ $(() => {
   $('#show-pcs').on('click', authEvents.onShowPCs)
   $('#edit-pc-name').on('submit', authEvents.onEditPCName)
   $('#back-to-pcs').on('click', authEvents.onShowPCs)
-  $('#back-to-pc').on('click', authEvents.onGoBackShowPC)
+  $('.back-to-pc').on('click', authEvents.onGoBackShowPC)
   $('.content-plant-collections').on('click', '.btn-pc-delete', authEvents.onDeletePC)
   $('.content-plants').on('click', '.btn-plant-delete', authEvents.onDeletePlant)
   $('.content-plant-collections').on('click', '.pc', authEvents.onShowPC)
