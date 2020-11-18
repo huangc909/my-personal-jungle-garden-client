e8098c8bdf6e74557769c9f0950a472cae8bfc51
Christine <huangc909@gmail.com>
2020-11-17 22:14:01 -0500

    Test production code with git.heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://afternoon-hamlet-91716.herokuapp.com',
-  // production: 'https://git.heroku.com/afternoon-hamlet-91716.git',
+  // production: 'https://afternoon-hamlet-91716.herokuapp.com',
+  production: 'https://git.heroku.com/afternoon-hamlet-91716.git',
   development: 'http://localhost:4741'
 }
 
